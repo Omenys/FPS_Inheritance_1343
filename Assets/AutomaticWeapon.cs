edb2248cfc5291e8951e48524163f19057153ab0
Omenys <105769289+Omenys@users.noreply.github.com>
2024-04-03 10:21:24 -0500

    Automatic Weapon Functional

---
+++ Assets/AutomaticWeapon.cs
@@ -0,0 +1,17 @@
+public class AutomaticWeapon : Gun
+{
+    public override bool AttemptFire()
+    {
+        if (!base.AttemptFire())
+            return false;
+
+        var b = Instantiate(bulletPrefab, gunBarrelEnd.transform.position, gunBarrelEnd.rotation);
+        b.GetComponent<Projectile>().Initialize(1, 100, 2, 2, null); // version without special effect
+
+        anim.SetTrigger("shoot");
+        elapsed = 0;
+        ammo -= 1;
+
+        return true;
+    }
+}
