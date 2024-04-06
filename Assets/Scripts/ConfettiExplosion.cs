3db454f65f6ca305c7a097cb1d3692cf953e64e6
Omenys <105769289+Omenys@users.noreply.github.com>
2024-04-05 23:48:17 -0500

    Confetti Gun Updates
    - New skin
    - Shorter burst time
    - Instances delete after particle system timer
    - Disabled OnDestroy() so multiple instances aren't called at the same time

--- Assets/Scripts/ConfettiExplosion.cs
+++ Assets/Scripts/ConfettiExplosion.cs
@@ -5,12 +5,13 @@ public class ConfettiExplosion : MonoBehaviour
     public ParticleSystem confetti;
 
     // Call particle system when projectile destroyed
-    private void OnDestroy()
+    /*private void OnDestroy()
     {
         ExplodeConfetti(transform.position);
 
-    }
+    }*/
 
+    // Call particle system when collision occurs
     public void OnTriggerEnter(Collider other)
     {
         ExplodeConfetti(transform.position);
@@ -19,6 +20,12 @@ public class ConfettiExplosion : MonoBehaviour
     // Play particle system at position that projectile collides
     private void ExplodeConfetti(Vector3 position)
     {
-        Instantiate(confetti, position, Quaternion.identity).Play();
+        ParticleSystem confettiClone = Instantiate(confetti, position, Quaternion.identity);
+        confettiClone.Play();
+        float confettiTimer = confettiClone.main.duration;
+        Destroy(confettiClone.gameObject, confettiTimer);
     }
+
 }
+
+
