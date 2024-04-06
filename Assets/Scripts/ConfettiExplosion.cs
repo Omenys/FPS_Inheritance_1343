5eeef6a86b98b461f456cc99c8c73ad5e4dab314
Omenys <105769289+Omenys@users.noreply.github.com>
2024-04-05 20:31:14 -0500

    Confetti Gun Works!

--- Assets/Scripts/ConfettiExplosion.cs
+++ Assets/Scripts/ConfettiExplosion.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 public class ConfettiExplosion : MonoBehaviour
 {
-    public ParticleSystem confettiEffect;
+    public ParticleSystem confetti;
+
+    // Call particle system when projectile destroyed
     private void OnDestroy()
     {
+        ExplodeConfetti(transform.position);
 
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        ExplodeConfetti(transform.position);
+    }
+
+    // Play particle system at position that projectile collides
+    private void ExplodeConfetti(Vector3 position)
+    {
+        Instantiate(confetti, position, Quaternion.identity).Play();
     }
 }
